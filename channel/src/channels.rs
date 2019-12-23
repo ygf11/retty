a682c99735a332571e518e4ccbe6bbb63ea1103d
yanggaofeng <yanggf23@gmail.com>
2019-12-23 17:26:36 +0800

    add server channel

--- channel/src/channels.rs
+++ channel/src/channels.rs
@@ -1,4 +1,4 @@
-use std::net::TcpStream;
+use std::net::{TcpStream, TcpListener};
 
 /// channel trait
 pub trait Channel{
@@ -22,6 +22,33 @@ impl SocketChannel{
 }
 
 impl Channel for SocketChannel{
+    fn get(&self){
+
+    }
+
+    fn read(&self){
+
+    }
+
+    fn write(&self){
+
+    }
+}
+
+pub struct ServerChannel{
+    channel: TcpListener,
+}
+
+impl ServerChannel {
+    pub fn new(channel:TcpStream) -> SocketChannel{
+        SocketChannel{
+            channel
+        }
+    }
+}
+
+
+impl Channel for ServerChannel{
     fn get(&self){
 
     }
