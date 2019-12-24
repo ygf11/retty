a41355821eb3cd77934f9052731b5f9e9be3feca
yanggaofeng <yanggf23@gmail.com>
2019-12-24 19:36:02 +0800

    fix up

--- channel/src/context.rs
+++ channel/src/context.rs
@@ -1,6 +1,14 @@
 use self::super::channels::Channel;
 
-struct Context{
-    buffer:Vec<u8>,
+/// inbound context
+/// read event
+struct InboundContext{
+    read_buffer:Vec<u8>,
+}
 
+
+/// outbound context
+/// write event
+struct OutboundContext{
+    write_buffer:Vec<u8>,
 }
