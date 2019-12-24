fe941cae217e742b61e90e0953937df1ec8d2f19
yanggaofeng <yanggf23@gmail.com>
2019-12-24 16:09:46 +0800

    add Node methods

--- channel/src/pipeline.rs
+++ channel/src/pipeline.rs
@@ -1,28 +1,40 @@
 use self::super::handlers::Handler;
 
 /// channel pipeline
-pub struct PipeLine{
+pub struct PipeLine {
     head: Option<*mut Node>,
     tail: Option<*mut Node>,
 }
 
 impl PipeLine {
-    fn new() -> PipeLine{
-        PipeLine{
-            head:None,
-            tail:None
+    fn new() -> PipeLine {
+        PipeLine {
+            head: None,
+            tail: None,
         }
     }
 }
 
 
-struct Node{
-    handler: Box<dyn Handler>,
+struct Node {
     prev: Option<*mut Node>,
     next: Option<*mut Node>,
+    handler: Box<dyn Handler>,
 }
 
+impl Node{
+    fn new(handler: Box<dyn Handler>) -> Node{
+        Node{
+            handler,
+            prev:None,
+            next:None,
+        }
+    }
 
+    fn into_element(self:Box<Self>) -> impl Handler{
+        self.handler
+    }
+}
 
 
 
