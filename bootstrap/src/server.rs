d3abb3cd7ceac6c728202fd4a951aa3739207926
yanggaofeng <yanggf23@gmail.com>
2019-12-25 15:13:57 +0800

    add bind logic

---
+++ bootstrap/src/server.rs
@@ -0,0 +1,32 @@
+use std::mem;
+use std::sync::mpsc::{Sender, channel};
+use std::thread::{Builder, Thread};
+
+struct ServerBootStrap {
+    sender: Option<Sender<Box<dyn FnOnce() -> ()>>>,
+}
+
+impl ServerBootStrap {
+    fn new() -> ServerBootStrap {
+        ServerBootStrap {
+            sender: None,
+        }
+    }
+
+    /// send msg to thread
+    fn bind(&mut self) {
+        let result = self.sender.take().or_else(|| {
+            let (sender, receiver) = channel();
+
+            let builder = Builder::new().spawn(|| {
+                // init logic
+            });
+
+            Some(sender)
+        });
+
+        self.sender = result;
+    }
+
+    fn close(&mut self) {}
+}
